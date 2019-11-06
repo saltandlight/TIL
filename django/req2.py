307856c78c9b02db3e5df6f121de197dbe934ebd
saltandlight <loverdevelop15@gmail.com>
2019-11-06 18:25:20 +0900

    req2.py 수정

--- django/req2.py
+++ django/req2.py
@@ -2,7 +2,7 @@
 import requests
 #2. url의 주소를 저장한다.
 url2 = "https://ticket.melon.com/common/ajax/offer"
-url="https://m.ticket.melon.com/main/index.htm"
+url="https://ticket.melon.com/offer/ajax/offerList.json?offerPosType=MAIN_B_CO_3"
 #3. 요청을 보내고 응답을 변수에 저장한다.
 headers = {
   'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Mobile Safari/537.36'
