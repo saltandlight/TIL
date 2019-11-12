36460e26990419920b3ad83e940058419074a571
saltandlight <loverdevelop15@gmail.com>
2019-11-12 11:13:06 +0900

    django

--- django/20191112/day5/lotto/views.py
+++ django/20191112/day5/lotto/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 # 무조건 첫째 파라미터는 request가 되어야 함
 def lotto(request):
-    
+
     return render(request, 'lotto.html')
 
 def winning(request):
+    # 1. 1~45까지의 숫자 중에 n개의 숫자를 랜덤 추출
+    # 1-1.  1~45까지의 번호를 가진 배열을 만들기
+    num_list = list(range(1,46))
+    num_count = request.GET['count']
+    #무조건 던지는 애는 다 스트링임(문자열로 인식)
 
-    return render(request, 'winning.html')
+    # 1-2. 해당 배열에서 count만큼의 숫자를 샘플링
+    result = random.sample(num_list, int(num_count)) 
+    # 2. 로또 당첨번호 공개 사이트로 가서 지난 주 당첨번호 가져오기
+    # - 몇회차인지, 언제 당첨번호인지, 1등 당첨금이 얼마인지
+
+    return render(request, 'winning.html', {'result':result})
