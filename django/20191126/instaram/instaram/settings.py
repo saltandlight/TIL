6ae7170ee7d21392e2c04ca12023e4cbcfb01d40
HyeBeen <31886887+saltandlight@users.noreply.github.com>
2020-03-31 11:19:43 +0900

    Update settings.py

--- django/20191126/instaram/instaram/settings.py
+++ django/20191126/instaram/instaram/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'hs#w1t5c&cbdt10-ont#%0iz@zur_&7e&ix2jdj&064=346%cs'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
